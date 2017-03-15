f04fba25c08d9a4c390f6523d56cbdeaa7dfc724
Gonzalo Montiel <gon.montiel@gmail.com>
2017-03-15 13:26:21 -0300

    Update department indexes

--- app/model/Department.js
+++ app/model/Department.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var tokenize = require('app/lib/strings.js').tokenize;
 
 /**
  * Representa un departamento
@@ -9,17 +10,34 @@ var departmentSchema = new Schema({
     'altCode': String,
     'contacto': String,
     'name': String,
+    'indexableTokens': String,
     'mailto': String,
     'CA-docentes': String,
     'CA-auxiliares': String,
     'CA-graduados': String,
     'CA-alumnos': String
+},
+{
+    toJSON: {
+        transform: function(doc, ret) {
+            delete ret.indexableTokens;
+            delete ret._id;
+        }
+    }
 });
 departmentSchema.index({code: 1}); // To search by code
 departmentSchema.index({altCode: 1}); // To search by alternative code
+departmentSchema.index({'indexableTokens': "text"});
 
 departmentSchema.methods.getConditions = function() {
     return {code: this.code};
 };
+
+departmentSchema.pre('save', true, function(next, done) {
+    // calling next kicks off the next middleware in parallel
+    next();
+    this.indexableTokens = tokenize(this.name);
+    done();
+});
 
 module.exports = mongoose.model('Department', departmentSchema);
