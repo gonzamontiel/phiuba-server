be42f5a9d8d2b7457a266b377f89d9cf37ec0e18
Gonzalo Montiel <gon.montiel@gmail.com>
2017-03-15 13:24:18 -0300

    Update models

--- app/model/Event.js
+++ app/model/Event.js
@@ -1,22 +1,32 @@
 var mongoose = require('mongoose');
+var tokenize = require('app/lib/strings.js').tokenize;
 
 /**
  * Representa un evento del calendario
  */
 var Schema = mongoose.Schema;
 var eventSchema = new Schema({
-    title: String,
-    start: Date,
-    end: Date,
-    parsedDate: Date,
-    extra: {
-      image: String,
-      info: String
+    'title': String,
+    'indexableTokens': String,
+    'start': Date,
+    'end': Date,
+    'parsedDate': Date,
+    'extra': {
+        'image': String,
+        'info': String
+    }
+},
+{
+    toJSON: {
+        transform: function(doc, ret) {
+            delete ret.indexableTokens;
+            delete ret._id;
+        }
     }
 });
 eventSchema.index({title: 1}, {unique: true}); // To search by title
 eventSchema.index({start: 1, end: 1}); // To search by dates
-eventSchema.index({"title": "text", "extra.info": "text"}); // Search by text
+eventSchema.index({"indexableTokens": "text", "extra.info": "text"}); // Search by text
 
 eventSchema.methods.getConditions = function() {
     if (this.title !== undefined) {
@@ -24,5 +34,12 @@ eventSchema.methods.getConditions = function() {
     }
     return this;
 };
+
+eventSchema.pre('save', true, function(next, done) {
+    // calling next kicks off the next middleware in parallel
+    next();
+    this.indexableTokens = tokenize(this.title);
+    done();
+});
 
 module.exports = mongoose.model('Event', eventSchema);
--- app/model/News.js
+++ app/model/News.js
@@ -1,23 +1,41 @@
 var mongoose = require('mongoose');
+var tokenize = require('app/lib/strings.js').tokenize;
 
 /**
  * Representa una noticia de la página principal de la FIUBA
  */
 var Schema = mongoose.Schema;
 var newsSchema = new Schema({
-    title: {type: String, default: 'notitle'},
-    created: Date,
-    thumbnail: String,
-    text: String,
-    img: String,
-    link: String
+    'title': {type: String, default: 'notitle'},
+    'created': Date,
+    'indexableTokens': String,
+    'thumbnail': String,
+    'text': String,
+    'img': String,
+    'link': String
+},
+{
+    toJSON: {
+        transform: function(doc, ret) {
+            delete ret.indexableTokens;
+            delete ret._id;
+        }
+    }
 });
+
 newsSchema.index({link: 1}, {unique: true}); // To avoid duplicates
-newsSchema.index({ "title": "text", "text": "text" }, {name: "newsTextIndex"}); // To search by text in title and description
+newsSchema.index({ "indexableTokens": "text", "text": "text" }, {name: "newsTextIndex"}); // To search by text in title and description
 newsSchema.index({created: -1}); // To search by date
 
 newsSchema.methods.getConditions = function() {
     return {'link': this.link};
 };
+
+newsSchema.pre('save', true, function(next, done) {
+    // calling next kicks off the next middleware in parallel
+    next();
+    this.indexableTokens = tokenize(this.title);
+    done();
+});
 
 module.exports = mongoose.model('News', newsSchema);
