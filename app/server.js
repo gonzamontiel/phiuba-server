e3851643a0b26a4ff401a321e67e89dcd819074b
Gonzalo Montiel <gon.montiel@gmail.com>
2017-04-02 13:49:05 -0300

    Add parameter to get  courses by dep

--- app/server.js
+++ app/server.js
@@ -80,6 +80,11 @@ app.get('/api/courses', function (req, res) {
                 if (error) Logger.error(error);
                 res.send(JSON.stringify(result));
             });
+    } else if (req.query.depCode) {
+        Course.find({'depCode': req.query.depCode}, {}, {sort: {name: 1}}, function(error, results) {
+            if (error) res.send(jsonError('Could not find data.'));
+            res.send(JSON.stringify(results));
+        });
     } else {
         Course.find({'planCode': req.query.planCode}, {}, {sort: {name: 1}}, function(error, results) {
             if (error) res.send(jsonError('Could not find data.'));
